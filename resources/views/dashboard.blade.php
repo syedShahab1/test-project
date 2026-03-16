e316ddcb16661cbff93092df553beb0e3b7cf04c
Shahab <syedshahabalam95@gmail.com>
2026-03-16 12:44:21 -0400

    test commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Test Project') }}
+            {{ __('Test 2 Project') }}
         </h2>
     </x-slot>
 
